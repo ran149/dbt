33b658690b6675ea8c6720c7fdd98061c2934276
lranjith.rl.340@gmail.com <lranjith.rl.340@gmail.com>
2023-12-26 18:04:19 +0000

    created fact_orders.sql and included fact_ordes.sql in dim_customer.sql

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -1,44 +1,39 @@
-with customers as (
+with
+    customers as (select * from {{ ref("stg_customers") }}),
 
-    select * from {{ ref('stg_customers') }}
+    orders as (select * from {{ ref("fact_orders") }}),
 
-),
+    customer_orders as (
 
-orders as (
+        select
+            customer_id,
+            min(order_date) as first_order_date,
+            max(order_date) as most_recent_order_date,
+            count(order_id) as number_of_orders,
+            sum(amount) as lifetime_value
 
-    select * from {{ ref('stg_orders') }}
+        from orders
 
-),
+        group by 1
 
-customer_orders as (
+    ),
 
-    select
-        customer_id,
+    final as (
 
-        min(order_date) as first_order_date,
-        max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
+        select
+            customers.customer_id,
+            customers.first_name,
+            customers.last_name,
+            customer_orders.first_order_date,
+            customer_orders.most_recent_order_date,
+            coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+            customer_orders.lifetime_value
 
-    from orders
+        from customers
 
-    group by 1
+        left join customer_orders using (customer_id)
 
-),
+    )
 
-final as (
-
-    select
-        customers.customer_id,
-        customers.first_name,
-        customers.last_name,
-        customer_orders.first_order_date,
-        customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
-
-    from customers
-
-    left join customer_orders using (customer_id)
-
-)
-
-select * from final
+select *
+from final
--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -1,12 +1,9 @@
-with customers as (
+with
+    customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
+        select id as customer_id, first_name, last_name from raw.jaffle_shop.customers
 
-    from raw.jaffle_shop.customers
+    )
 
-)
-
-select * from customers
+select *
+from customers
